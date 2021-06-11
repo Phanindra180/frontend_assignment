d5f4ecd12d9eeaf25554cc6342ced6777788c160
Phanindra180 <phanindrasai180@gmail.com>
2021-06-11 11:28:58 +0530

    assign_submission2

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 // console.log("entered script");
 // mouse listeners
 const changeImage = function (element) {
-  return () => {
-    // console.log("entered func");
-    for (let i = 0; i < items.length; i++) {
-      if (items[i] == element) index = i;
-    }
-    element.classList.toggle("container_active");
-    currently_active.classList.toggle("container_active");
-    currently_active = element;
+  // console.log("entered func");
+  for (let i = 0; i < items.length; i++) {
+    if (items[i] == element) index = i;
+  }
+  element.classList.toggle("container_active");
+  currently_active.classList.toggle("container_active");
+  currently_active = element;
 
-    const bigImg = document.querySelector(".bigImage");
-    const elt_img = element.querySelector("img");
-    bigImg.src = elt_img.src;
+  const bigImg = document.querySelector(".bigImage");
+  const elt_img = element.querySelector("img");
+  bigImg.src = elt_img.src;
 
-    const title = element.querySelector(".title");
-    const cap = document.querySelector("figcaption");
-    cap.innerText = title.innerText;
-  };
+  const title = elt_img.title;
+  const cap = document.querySelector(".caption");
+  cap.innerText = title;
+  truncate(cap);
 };
 
 const cat = document.querySelector("#cat");
@@ -28,11 +27,21 @@ const NextByk = document.querySelector("#NextByk");
 const interns = document.querySelector("#interns");
 var currently_active = cat;
 
-cat.addEventListener("click", changeImage(cat));
-couple.addEventListener("click", changeImage(couple));
-bali.addEventListener("click", changeImage(bali));
-NextByk.addEventListener("click", changeImage(NextByk));
-interns.addEventListener("click", changeImage(interns));
+cat.addEventListener("click", function () {
+  changeImage(cat);
+});
+couple.addEventListener("click", function () {
+  changeImage(couple);
+});
+bali.addEventListener("click", function () {
+  changeImage(bali);
+});
+NextByk.addEventListener("click", function () {
+  changeImage(NextByk);
+});
+interns.addEventListener("click", function () {
+  changeImage(interns);
+});
 
 //keyboard listeners
 const items = document.querySelectorAll("li");
@@ -40,13 +49,10 @@ const items = document.querySelectorAll("li");
 var index = 0;
 
 const changeImage_key = function (eve) {
-  console.log(eve.key);
   if (eve.key == "ArrowUp") {
-    console.log("done");
     index--;
     if (index < 0) index++;
   } else if (eve.key == "ArrowDown") {
-    console.log("donedone");
     index++;
     if (index == items.length) index--;
   }
@@ -56,25 +62,21 @@ window.addEventListener("keydown", function (Event) {
   changeImage_key(Event);
   console.log(items[index]);
   let element = items[index];
-  element.classList.toggle("container_active");
-  currently_active.classList.toggle("container_active");
-  currently_active = element;
-
-  const bigImg = document.querySelector(".bigImage");
-  const elt_img = element.querySelector("img");
-  bigImg.src = elt_img.src;
-
-  const title = element.querySelector(".title");
-  const cap = document.querySelector("figcaption");
-  cap.innerText = title.innerText;
+  changeImage(element);
 });
 
 //title modification
+const truncate = function (item) {
+  while (item.clientWidth < item.scrollWidth) {
+    let text = item.innerText;
+    // console.log(text, item.clientWidth, item.scrollWidth);
+    let len = text.length;
+    text = text.substr(0, len / 2 - 2) + "..." + text.substr(len / 2 + 2, len);
+    item.innerText = text;
+  }
+};
+
 const title_items = document.querySelectorAll(".title");
 for (let i = 0; i < title_items.length; i++) {
-  let text = title_items[i].innerText;
-  if (text.length > 35) {
-    title_items[i].innerText =
-      text.substr(0, 14) + "..." + text.substr(text.length - 13, text.length);
-  }
+  truncate(title_items[i]);
 }
